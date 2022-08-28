7455004562b9cbe256457b170b215d6fa5227a7c
popTheSmart <popthesmart@hotmail.com>
2022-08-28 13:24:25 -0400

    add working ai prediction

--- sketch.js
+++ sketch.js
@@ -1,12 +1,18 @@
-
-var player_move = null; // 1 = rock, 2 = paper, 3 = scissors
 var games_played = 0;
 var games_won = 0;
 var games_lost = 0;
+var games_tied = 0;
 
+var agent_move = 1;
 const net = new brain.recurrent.LSTMTimeStep();
-data = [1, 2];
+data = [1, 2, 3];  // 1 = rock, 2 = paper, 3 = scissors
 
+// converts the predicted player move to agent move
+const convert_to_agent_move = {
+    1: 2,
+    2: 3,
+    3: 1
+}
 
 // Ran once when the sketch is loaded
 function setup() {
@@ -18,7 +24,47 @@ function draw() {
     background(50);
 }
 
-function play_round() {
+function keyPressed() {
+    switch (key) {
+        case '1':
+            player_move = 1;
+            play_round(player_move);
+            break;
+        case '2':
+            player_move = 2;
+            play_round(player_move);
+            break;
+        case '3':
+            player_move = 3;
+            play_round(player_move);
+            break;
+    }
+}
+
+function play_round(player_move) {
+    if (agent_move == player_move) {
+        console.log('Tie!');
+        games_tied++;
+    }
+    else if ((agent_move == 1 && player_move == 2) || (agent_move == 2 && player_move == 3) || (agent_move == 3 && player_move == 1)) {
+        console.log('You win!');
+        games_won++;
+    }
+    else {
+        console.log('You lose!')
+        games_lost++;;
+    }
+
+    games_played++;
+
+    agent_move = predict_move(player_move);
+    agent_move = round(agent_move)
+    agent_move = convert_to_agent_move[agent_move];
+    //console.log(agent_move);
+    console.log("games played: " + games_played, "games won: " + games_won, "games lost: " + games_lost, "games tied: " + games_tied);
+}
+
+function predict_move(player_move) {
     data.push(player_move);
 
     if (data.length > 10) {
@@ -29,25 +75,8 @@ function play_round() {
 
     net.train(
         [data],
-        { iterations: 100, log: true }
+        { iterations: 100, log: false }
     );
 
-    result = net.run(data);
-    console.log(result);
+    return net.run(data); // returns predicted move, will be a float in range of 1-3, occasionly 4
 }
-
-function keyPressed() {
-    switch (key) {
-        case '1':
-            player_move = 1;
-            break;
-        case '2':
-            player_move = 2;
-            break;
-        case '3':
-            player_move = 3;
-            break;
-    }
-
-    play_round();
-  }
